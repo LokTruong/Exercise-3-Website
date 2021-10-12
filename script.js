ce4b7596056a072743cb13abbe5e5b7bc76eb187
LokTruong <truong18@purdue.edu>
2021-10-12 18:25:56 -0400

    added animations

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-// Event handing for scrolling animations
-function isElementInViewport(elem) {
-    var $elem = $(elem);
+// // Event handing for scrolling animations
+// function isElementInViewport(elem) {
+//     var $elem = $(elem);
+// }
 
-    // Get the scroll position of the page.
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+// Looking at Stack Overflow approach towards scroll position events
 
-    // Get the position of the element on the page.
-    var elemTop = Math.round( $elem.offset().top );
-    var elemBottom = elemTop + $elem.height();
+//     // Get the scroll position of the page.
+//     var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+//     var viewportTop = $(scrollElem).scrollTop();
+//     var viewportBottom = viewportTop + $(window).height();
 
-    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-}
+//     // Get the position of the element on the page.
+//     var elemTop = Math.round( $elem.offset().top );
+//     var elemBottom = elemTop + $elem.height();
 
-// Check if it's time to change the state.
-function checkState() {
-    var $elem = $('.model_S .model_Y .model_3 .model_X .solar_panels .solar_roof .accessories');
+//     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+// }
 
-    // If the state hasn't already been changed
-    if ($elem.hasClass('start'))
-        return;
+// // Check if it's time to change the state.
+// function checkState() {
+//     var $elem = $('.model_S .model_Y .model_3 .model_X .solar_panels .solar_roof .accessories');
 
-    if (isElementInViewport($elem)) {
-        // Change the state
-        $elem.addClass('start');
-    }
-}
+//     // If the state hasn't already been changed
+//     if ($elem.hasClass('start'))
+//         return;
 
-// Capture scroll events
-$(window).scroll(function(){
-    checkState();
-});
+//     if (isElementInViewport($elem)) {
+//         // Change the state
+//         $elem.addClass('start');
+//     }
+// }
+
+// // Capture scroll events
+// $(window).scroll(function(){
+//     checkState();
+// });
